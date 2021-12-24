66f128f5d7a7fd910707aa60447b33a0c6a7f5e8
Seok Ji Woo <seokjw0727@naver.com>
2021-12-25 02:58:50 +0900

    seconds commit

---
+++ importme.py
@@ -0,0 +1,4 @@
+def 지우개():
+    for i in range(100):
+        print()
+    print("지우개를 사용하여 코드를 지웠습니다.")
---
+++ test.py
@@ -0,0 +1,10 @@
+# 그냥 하고싶은거 하는 거임
+
+import importme
+
+
+print("안녕하세요.")
+importme.지우개()
+
+
+
