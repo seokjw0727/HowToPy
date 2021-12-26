f82028b70d158e3120293f023399a56557cac7d5
Seok Ji Woo <seokjw0727@naver.com>
2021-12-26 21:39:35 +0900

    Fifth commit

---
+++ Modify Strings.py
@@ -0,0 +1,23 @@
+a = "Hello, World!" 
+print(a.upper())
+
+# 변수를 모두 대문자로 변환하여 출력함
+
+print(a.lower())
+
+# 변수를 모두 소문자로 변환하여 출력함
+
+a_with_space = "          Hello, World!           "
+print(a_with_space.strip())
+
+# 변수의 양 끝에 있는 공백을 모두 지워줌
+
+print(a.replace("H", "J"))
+
+# 파라미터를 사용하여 변수 내부의 값을 변환하여줌
+
+print(a.split(","))
+
+# 파라미터의 내용을 변수 내부에서 찾아, 내용을 분할시켜줌
+
+
