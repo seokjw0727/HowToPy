d021f758a3956690abb7cbcaaf42ad6b5742c38c
Seok Ji Woo <seokjw0727@naver.com>
2022-02-03 18:20:48 +0900

    백준

--- BOJ.py
+++ BOJ.py
@@ -332,12 +332,12 @@
 
 # print('{} {}'.format(min(b), max(b)))
 
-num_list = []
-for i in range(9):
-    num_list.append(int(input()))
+# num_list = []
+# for i in range(9):
+#     num_list.append(int(input()))
     
-print(max(num_list))
-print(num_list.index(max(num_list))+1)
+# print(max(num_list))
+# print(num_list.index(max(num_list))+1)
 
 
 
